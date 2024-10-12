4e35c02eafe4afbdf4d7afc9c1a08266b8694f69
elleszcd <gragasinrochelle70@gmail.com>
2024-10-12 14:52:46 +0800

    Add bullet Skills

---
+++ src/Skills.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>
+      <ul>
+        <li>HTML</li>
+        <li>JAVA</li>
+        <li>CSS</li>
+        <li>PYTHON</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Skills
