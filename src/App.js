d8cf1cbd3278d0bc1ec1227a62d382fc1efc6314
elleszcd <gragasinrochelle70@gmail.com>
2024-10-12 14:40:52 +0800

    Add heading and navigation home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Home from './Home'
-import { Route, Router, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' Home />
-      </Routes>
-    </Router>
+    <div>
+      <a href='/'>Home</a>
+      <Home></Home>
+    </div>
   )
 }
 
