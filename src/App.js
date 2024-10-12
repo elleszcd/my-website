5ad7b27724f2c09d8831625119552e49b6cc3be3
elleszcd <gragasinrochelle70@gmail.com>
2024-10-12 14:49:29 +0800

    Add Skills

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './Home'
+import Skills from './Skills'
 
 const App = () => {
   return (
     <div>
       <a href='/'>Home</a>
       <Home></Home>
+      <Skills></Skills>
     </div>
   )
 }
